983378792765f375f02e1bfb86692c45f20b6cc6
diar2705 <diar.bathish@campus.technion.ac.il>
2024-02-16 13:43:09 +0200

    ex2 should work (didnt test)

--- ex2.asm
+++ ex2.asm
@@ -1,5 +1,34 @@
-.global _start
+    .global_start
 
-.section .text
+    .section.text
 _start:
-#your code here
+    #your code here
+    xor %rsi, %rsi
+
+forloop_HW1:
+    cmp %rsi, $9
+    jge end_forloop_HW1
+    movq vertices(, %rsi, 8), %rax
+    movb %rdi $1
+
+whileloop_HW1:
+    movq 0(%rax, %rdi, 8), %rbx
+    cmp %rbx, $0
+    je  end_whileloop_HW1
+    cmp %rbx, %rax
+    je  set_circle_HW1
+    cmp %rdi, $9
+    jge set_circle_HW1
+    inc %rdi
+    jmp whileloop_HW1
+
+end_whileloop_HW1:
+    inc %rsi
+    jmp forloop_HW1
+
+end_forloop_HW1:
+    movb $-1, (circle)
+    ret
+set_circle_HW1:
+    movb $1, (circle)
+    ret
