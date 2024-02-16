6ba2f7022ac32149cef309bcdd3fdbadcaffb6a3
diar2705 <diar.bathish@campus.technion.ac.il>
2024-02-16 14:46:42 +0200

    fixed small thing

--- ex2.asm
+++ ex2.asm
@@ -3,27 +3,27 @@
     .section.text
 _start:
     #your code here
-    xor %rsi, %rsi
+    xor %edx, %edx
 
 forloop_HW1:
-    cmp %rsi, $9
+    cmp %edx, $9
     jge end_forloop_HW1
-    movq vertices(, %rsi, 8), %rax
-    movb %rdi $1
+    movq vertices(, %edx, 8), %rax
+    movl %ecx $1
 
 whileloop_HW1:
-    movq 0(%rax, %rdi, 8), %rbx
+    movq 0(%rax, %ecx, 8), %rbx
     cmp %rbx, $0
     je  end_whileloop_HW1
     cmp %rbx, %rax
     je  set_circle_HW1
-    cmp %rdi, $9
+    cmp %ecx, $9
     jge set_circle_HW1
-    inc %rdi
+    inc %ecx
     jmp whileloop_HW1
 
 end_whileloop_HW1:
-    inc %rsi
+    inc %edx
     jmp forloop_HW1
 
 end_forloop_HW1:
