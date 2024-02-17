6f1a1fe7f9a8967f048ca240e5f969097edfe572
diar2705 <diar.bathish@campus.technion.ac.il>
2024-02-17 16:14:48 +0200

    .

---
+++ a.cpp
@@ -0,0 +1,12 @@
+
+
+int main()
+{
+
+    int size;
+    int source_array[size], up_array[size], down_array[size];
+    for(int i = 0; i < size; i++)
+    {
+        if()
+    }
+}
--- ex3.asm
+++ ex3.asm
@@ -7,44 +7,44 @@ _start:
     movq $source_array, %rax
     movq $down_array, %rbx
     movq $up_array, %rcx
-    movl size, %rdx
+    movl size, %edx
 
     movl (%rax), %r8d                       # min value
-    xor %r11, %r11                          # index
+    xor %r11d, %r11d                          # index
 
     movl (%rax), %r9d                       # max value
-    xor %r12, %r12                          # index
+    xor %r12d, %r12d                          # index
 
 find_Min_HW1:
-    cmp %r11, %rdx
+    cmp %r11d, %edx
     jge find_Max_HW1
 
-    movl (%rax, %r11, 4), %r13d
+    movl (%rax, %r11d, 4), %r13d
     cmp %r13d, %r8d
     jl setLess_HW1
 
-    inc %r11
+    inc %r11d
     jmp find_Min_HW1
 
 setLess_HW1:
     movl %r13d, %r8d
-    inc %r11
+    inc %r11d
     jmp find_Min_HW1
 
 find_Max_HW1:
-    cmp %r12, %rdx
+    cmp %r12d, %edx
     jge continue_HW1
 
-    movl (%rax, %r11, 4), %r13d
+    movl (%rax, %r11d, 4), %r13d
     cmp %r13d, %r9d
     jg setBigger_HW1
 
-    inc %r12
+    inc %r12d
     jmp find_Max_HW1
 
 setBigger_HW1:
     cmp %r13d, %r9d
-    inc %r12
+    inc %r12d
     jmp find_Max_HW1
 
 
@@ -52,25 +52,25 @@ continue_HW1:
     dec %r8d
     inc %r9d
 
-    xor %r10, %r10                         # index
+    xor %r10d, %r10d                         # index
 
 forloop_HW1:
-    cmp %r10, %rdx
+    cmp %r10d, %edx
     jge sucess_HW1
 
-    movl (%rax, %r11, 4), %r13d
+    movl (%rax, %r11d, 4), %r13d
     cmp %r8d, %r13d
     jge insert_down_HW1
 
     cmp %r13d, %r9d
     jge insert_up_HW1
 
-    inc %r10
-    cmp %r10, %rdx
+    inc %r10d
+    cmp %r10d, %edx
     jge continue_loop_HW1
 
-    movl (%rax, %r10, 4), %r15d
-    dec %r10
+    movl (%rax, %r10d, 4), %r15d
+    dec %r10d
     cmp %r13d, %r15d
     jge insert_down_HW1
     jmp insert_up_HW1
