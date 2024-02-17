2a3f861c2a435714b9f61e986855330b6d4398f3
diar2705 <diar.bathish@campus.technion.ac.il>
2024-02-17 16:08:59 +0200

    .

--- ex3.asm
+++ ex3.asm
@@ -4,46 +4,47 @@
 _start:
 #your code here
 
-    xor %r10d, %r10d                        % index
+    movq $source_array, %rax
+    movq $down_array, %rbx
+    movq $up_array, %rcx
+    movl size, %rdx
 
-    movl $source_array, %rax
-    movl $up_array, %rbx
-    movl $down_array, %rcx
+    movl (%rax), %r8d                       # min value
+    xor %r11, %r11                          # index
 
-    movl (%rax), %r8d                       % min value
-    xor %r11d, %r11d                        % index
-
-    movl (%rax), %r9d                       % max value
-    xor %r12d, %r12d                        % index
+    movl (%rax), %r9d                       # max value
+    xor %r12, %r12                          # index
 
 find_Min_HW1:
-    cmp %r11d, size
+    cmp %r11, %rdx
     jge find_Max_HW1
 
-    cmp (%rax, %r11d, 4), %r8d
+    movl (%rax, %r11, 4), %r13d
+    cmp %r13d, %r8d
     jl setLess_HW1
 
-    add $4, %rax
+    inc %r11
     jmp find_Min_HW1
 
 setLess_HW1:
-    movl (%rax), %r8d
-    add $4, %rax
+    movl %r13d, %r8d
+    inc %r11
     jmp find_Min_HW1
 
 find_Max_HW1:
-    cmp %r11d, size
+    cmp %r12, %rdx
     jge continue_HW1
 
-    cmp (%rax), %r9d
+    movl (%rax, %r11, 4), %r13d
+    cmp %r13d, %r9d
     jg setBigger_HW1
 
-    add $4, %rax
+    inc %r12
     jmp find_Max_HW1
 
 setBigger_HW1:
-    movl (%rax), %r9d
-    add $4, %rax
+    cmp %r13d, %r9d
+    inc %r12
     jmp find_Max_HW1
 
 
@@ -51,6 +52,62 @@ continue_HW1:
     dec %r8d
     inc %r9d
 
+    xor %r10, %r10                         # index
 
 forloop_HW1:
-    
+    cmp %r10, %rdx
+    jge sucess_HW1
+
+    movl (%rax, %r11, 4), %r13d
+    cmp %r8d, %r13d
+    jge insert_down_HW1
+
+    cmp %r13d, %r9d
+    jge insert_up_HW1
+
+    inc %r10
+    cmp %r10, %rdx
+    jge continue_loop_HW1
+
+    movl (%rax, %r10, 4), %r15d
+    dec %r10
+    cmp %r13d, %r15d
+    jge insert_down_HW1
+    jmp insert_up_HW1
+
+
+continue_loop_HW1:
+    movl %r13d, (%rcx)
+    add $4, %rcx
+    jmp forloop_HW1
+
+insert_down_HW1:
+    cmp %r13d, %r9d 
+    jge fail_HW1
+
+    movl %r13d, (%rbx)
+    add $4, %rbx
+    jmp forloop_HW1
+
+insert_up_HW1:
+    cmp %r8d, %r13d
+    jge fail_HW1
+
+    movl %r13d, (%rcx)
+    add $4, %rcx
+    jmp forloop_HW1
+
+fail_HW1:
+    movb $0, (bool)
+    jmp end_HW1
+
+
+sucess_HW1:
+    movb $1, (bool)
+    jmp end_HW1
+
+
+end_HW1:
+
+
+
