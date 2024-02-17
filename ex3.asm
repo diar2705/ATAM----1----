6b04eb760e868a7d41ed537a5d665c4c44b2ab3d
diar2705 <diar.bathish@campus.technion.ac.il>
2024-02-17 15:18:19 +0200

    .

--- ex3.asm
+++ ex3.asm
@@ -3,28 +3,64 @@
 .section .text
 _start:
 #your code here
-    orl $0x7fffffff, %r8d
-    orl $0x80000000, %r9d
 
-    xor %r10d, %r10d                    ; counter for he loop (index for source_array)
 
-    movl source_array, %eax
-    movl up_array, %ebx
-    movl down_array, %ecx
+    xor %r10d, %r10d                   
+
+    movl $source_array, %eax
+    movl $up_array, %ebx
+    movl $down_array, %ecx
+
+    movl (%eax), %r12d
+    movl (%eax), %r13d
+    xor %r14d, %r14d
+forMAX_HW1:
+    cmpl %r14d, size
+    jmp forMIN_HW1
+    cmpl 0(%eax,%r14d,4), %r12d
+    jg bigger_HW1
+    inc %r14d
+    jmp forMAX_HW1
+
+bigger_HW1:
+    movl 0(%eax,%r14d,4), %r12d
+    inc %r14d
+    jmp forMAX_HW1
+
+
+    xor %r14d, %r14d
+forMIN_HW1:
+    cmpl %r14d, size
+    jmp begin_HW1
+    cmpl 0(%eax,%r14d,4), %r13d
+    jl smaller_HW1
+    inc %r14d
+    jmp forMIN_HW1
+
+smaller_HW1:
+    movl 0(%eax,%r14d,4), %r13d
+    inc %r14d
+    jmp forMIN_HW1
+
+begin_HW1:
+    inc %r12d
+    dec %r13d
 
 forloop_HW1:
     cmp %r10d, size
     jge sucsess_HW1
 
     cmp %r9d, 0(%eax)
-    jge inc_HW1
-    cmp 0(%eax), %r8d
     jge dec_HW1
+    cmp 0(%eax), %r8d
+    jge inc_HW1
 
     inc %r10d
     cmp %r10d, size
     jge continue_HW1
-    cmp 0(%eax), 4(%eax)
+    movl 0(%eax), %r11d
+    dec %r10d
+    cmp %r11d, 4(%eax)
     jge dec_HW1
     jmp inc_HW1
 
@@ -53,8 +89,12 @@ dec_HW1:
 
 sucsess_HW1:
     movb $1, bool
-    ret
+    jmp end
 
 failure_HW1:
     movb $0, bool
-    ret
+    jmp end
+
+end:
+
+    
